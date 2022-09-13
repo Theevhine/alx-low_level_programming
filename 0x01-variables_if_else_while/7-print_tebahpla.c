70c1ac9868517a2f5bfa80e78a50698d358ff377
Theevhine <divineukasoanya@gmail.com>
2022-09-12 18:20:15 -0700

    work

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,13 @@
-#include
+#include <stdio.h>
+/**
+* main-program entry point.
+* Return:0 if no errors, non zero if errors
+*/
+int main(void)
+{
+char i;
+for (i = 'z'; i >= 'a'; i--)
+putchar(i);
+putchar('\n');
+return (0);
+}
